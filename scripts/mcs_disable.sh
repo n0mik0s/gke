8dd56b694ccf350e3ee69668fbe7affa471615a7
Kalinichenko Vitalii <kalinichenko.vitalik@gmail.com>
2021-07-02 10:27:17 +0300

    Alpha release are redy to go:)

--- scripts/mcs_disable.sh
+++ scripts/mcs_disable.sh
@@ -11,8 +11,6 @@ gcloud container hub memberships unregister $GKE_1_name \
 gcloud container hub memberships unregister $GKE_2_name \
    --gke-cluster $GKE_2_zone/$GKE_2_name
 
-gcloud services disable gkeconnect.googleapis.com --project $PROJECT_ID
-gcloud services disable gkehub.googleapis.com --project $PROJECT_ID
 gcloud services disable trafficdirector.googleapis.com --project $PROJECT_ID
 gcloud services disable multiclusterservicediscovery.googleapis.com --project $PROJECT_ID
 gcloud alpha container hub multi-cluster-services disable --project $PROJECT_ID
