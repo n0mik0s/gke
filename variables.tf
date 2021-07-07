846f83b86bba5d5b875bd77fddc58c99329fd961
Mykyta_Demchenko <nekitdem@gmail.com>
2021-07-07 18:07:15 +0300

    create my branch

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "ping_cluster_1" {
 variable "ping_cluster_2" {
   type        = map(any)
   default     = {}
-  description = "The map of objects that should be the inputs for gke module"
+  description = "The +map of objects that should be the inputs for gke module"
 }
